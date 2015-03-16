19fef84bcca34294d8a18d4f73a0e3899beb7a42
theeeslam <tomislavsalamanov@gmail.com>
2015-03-16 15:37:06 -0400

    Start

---
+++ plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Plugin Name: Plugin
+ * Plugin URI: http://www.animalfactguide.com/wp-content/uploads/2013/10/ostrich2-585x585.jpg
+ * Description: Our Plugin
+ * Author: Paige, Tom, Elyse
+ * Version: 1.0
+ * Author URI: www.google.com
+ */
+ 
+ 
+ ?>
