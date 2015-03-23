8e7d86d5b8853e1dba7eef960bd693ebfb381616
theeeslam <tomislavsalamanov@gmail.com>
2015-03-23 17:57:49 -0400

    Adding a Menu Part Dos

--- plugin.php
+++ plugin.php
@@ -1,11 +1,9 @@
 <?php
 /*
  * Plugin Name: Plugin
- * Plugin URI: http://www.animalfactguide.com/wp-content/uploads/2013/10/ostrich2-585x585.jpg
  * Description: Our Plugin
  * Author: Paige, Tom, Elyse
  * Version: 1.0
- * Author URI: www.google.com
  */
  
 /** Step 2 (from text above). */
@@ -25,13 +23,7 @@ function my_plugin_options() {
 	echo '<p>Here is where the form would go if I actually had options.</p>';
 	echo '</div>';
 }
+
+
 ?>
  
- 
- 
- 
- 
- 
- 
- 
- ?>
